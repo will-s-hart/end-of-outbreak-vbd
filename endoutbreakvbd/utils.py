e927927f1865d2704236ab7454dbd072c605c043
will-s-hart <william.hart@maths.ox.ac.uk>
2026-01-23 14:55:15 +0000

    Fix for pandas 3 (need to ask for copy of array to modify)

--- endoutbreakvbd/utils.py
+++ endoutbreakvbd/utils.py
@@ -6,7 +6,7 @@ def month_start_xticks(ax, year=2017, interval_months=2):
     month_starts = pd.date_range(
         start=f"{year}-01-01", end=f"{year + 1}-01-01", freq=f"{interval_months}MS"
     )
-    month_starts_doy = month_starts.dayofyear.to_numpy()
+    month_starts_doy = month_starts.dayofyear.to_numpy(copy=True)
     month_starts_doy[-1] = 366
     xlim = ax.get_xlim()
     month_start_in_range = (month_starts_doy >= xlim[0]) & (
